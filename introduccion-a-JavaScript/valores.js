daa581055ce92e6bac13b93624c68b8a8fc0d293
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-23 20:00:54 -0300

    Agregando valor

--- introduccion-a-JavaScript/valores.js
+++ introduccion-a-JavaScript/valores.js
@@ -13,10 +13,18 @@ undefined
 // Objetos
 [1, 2, 3]
 {
-    nombre: 'Jeremias',
-    age: 19
+  nombre: 'Jeremias',
+  age: 19
 }
 
 // Hay tipos de datos primitivos, de objectos y "raros" por ejemplo
 // typeof undefined = undefined, typeof null = object, etc.
 // undefined se puede definir como el espacio (variable) que todavia no tiene un valor
+
+// Tambien existe otro tipo de dato que surge por ejemplo de hacer:
+1 / 0 = Infinity
+typeof Infinity = Number
+// se puede hacer por ejemplo:
+1 / Infinity = 0
+
+// Infinity al ser Number es un valor Primitivo y pertenece al scope global
