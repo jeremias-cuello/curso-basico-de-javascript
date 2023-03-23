b13414861fe1dd5675f032c2016678410bc3b07f
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-23 14:49:46 -0300

    agregando concepto de undefined

--- clase-03/valores.js
+++ clase-03/valores.js
@@ -19,3 +19,4 @@ undefined
 
 // Hay tipos de datos primitivos, de objectos y "raros" por ejemplo
 // typeof undefined = undefined, typeof null = object, etc.
+// undefined se puede definir como el espacio (variable) que todavia no tiene un valor
