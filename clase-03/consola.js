5dd178026afafe033d8545be9fe7d6e9ccfcc76d
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-23 13:23:01 -0300

    Clase 03 - Tipos de Datos

---
+++ clase-03/consola.js
@@ -0,0 +1,9 @@
+console.log(typeof 40);
+console.log(typeof Oscar);
+console.log(typeof "Oscar");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null); // objeto
+console.log(typeof []); // objeto
+console.log(typeof {}); // objeto
---
+++ clase-03/valores.js
@@ -0,0 +1,21 @@
+
+// Valores primitivos
+5
+
+'Jeremias Natanael Cuello'
+
+true
+false
+
+null
+undefined
+
+// Objetos
+[1, 2, 3]
+{
+    nombre: 'Jeremias',
+    age: 19
+}
+
+// Hay tipos de datos primitivos, de objectos y "raros" por ejemplo
+// typeof undefined = undefined, typeof null = object, etc.
