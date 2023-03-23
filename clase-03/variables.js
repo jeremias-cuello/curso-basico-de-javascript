489bad2af150a18f9aabd66a905518ae49a55242
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-23 14:50:48 -0300

    variables

---
+++ clase-03/variables.js
@@ -0,0 +1,20 @@
+var nombre = "Oscar";
+console.log(nombre);
+
+// declarar una variable
+var edad;
+console.log(edad); // undefined
+
+// inicialicar
+edad = 30;
+
+// el operador = es un operador de asignacion, asigna al miembro de la izquierda un valor que el miembro de la derecha
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+  nombre: 'Jeremias', // string
+  edad: 19 // number
+}
+
+console.log(persona);
+console.log(elementos[0]);
