e68013989ea169444ee7ef6655c84713ac9f87be
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-23 19:30:15 -0300

    booleans

---
+++ Bases-de-JavaScript/truthy-falsy.js
@@ -0,0 +1,11 @@
+var variable1 = "";
+console.log(Boolean(variable1)); // valor por defecto de Boolean() (false)
+/**
+ * CONCLUSION: para todo valor de variable1 igual a:
+ * 0, false, "", null y undefined
+ * Boolean(variable1) = false.
+ *
+ * Para todo valor distinto de ellos
+ * Boolean(variable1) = true
+ */
+
