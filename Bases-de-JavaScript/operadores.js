cced4a0de3740deff3d71e79e320b9a97bcc4549
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-25 18:15:02 -0300

    operadores

---
+++ Bases-de-JavaScript/operadores.js
@@ -0,0 +1,42 @@
+// operadores aritmeticos/binarios
+console.log(3 + 2)
+console.log(50 - 10)
+console.log(10 * 20)
+console.log(20 / 2)
+
+// operador de concatenacion (+)
+// JavaScript prioriza a + a los String
+console.log("Jeremias" + " Cuello")
+
+// operador de negacion - operador unitario (porque operamos un solo valor)
+console.log(!false);
+
+// operadores de asignacion
+var a = 1; // se le asigna a a -> 1
+
+// operador de comparacion
+console.log(3 == "3");
+
+// operador de comparacion estricto
+console.log(3 === "3");
+
+// operador de comparacion
+console.log(5 < 3);
+console.log(5 > 3);
+console.log(5 <= 6);
+console.log(5 >= 6);
+
+// operadores logicos
+// console.log(a && b);
+// console.log(a || b);
+
+// operadores de incremento
+var edad = 19;
+console.log('edad: ' + edad--);
+
+var edad = 19;
+console.log('edad: ' + ++edad);
+
+var edad = 19;
+edad += 2;
+console.log(edad);
