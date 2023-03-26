be7f746e7abf182b7dee10034717a2db3b0ace69
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-26 20:11:47 -0300

    playground

--- loops/loops.js
+++ loops/loops.js
@@ -22,3 +22,32 @@ while (estudiantes.length > 0) {
 // while (estudiante = estudiantes.shift()) {
 //   saludarEstudiantes(estudiante);
 // }
+
+// playground
+/**
+ * En este desafío tu función solution recibirá 3 parámetros:
+ * Tu función debe retornar un array de elementos con las siguientes indicaciones:
+ *
+ * Si el parámetro deathCount es igual a 0, debes retornar un array con los mismos
+ * elementos que tenía el array de estudiantes y agregando el elemento nuevo al final.
+ * Si el parámetro deathCount es mayor a 0, debes retornar un array con los mismos
+ * elementos del array estudiantes pero eliminando la cantidad de estudiantes que indica
+ * el número deathCount y agregando el elemento nuevo al final.
+ *
+ * @param {String[]} estudiantes nombres de los estudiantes
+ * @param {Number} deathCount
+ * @param {String} nuevo nombre del nuevo estudiante
+ * @returns nuevos estudiantes
+ */
+function solution(estudiantes, deathCount, nuevo) {
+  if (deathCount === 0) {
+    estudiantes.push(nuevo);
+    return estudiantes;
+  } else if (deathCount > 0) {
+    for (var i = 0; i < deathCount; i++){
+      estudiantes.pop();
+    }
+    estudiantes.push(nuevo);
+    return estudiantes;
+  }
+}
