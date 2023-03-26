5c3e36e65445e90e097c772f87908c8a69b89200
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-26 19:44:51 -0300

    loops. while.

--- loops/loops.js
+++ loops/loops.js
@@ -6,10 +6,19 @@ function saludarEstudiantes(estudiante) {
 
 // con for
 for (var i = 0; i < estudiantes.length; i++) {
-  saludarEstudiantes(estudiantes[i]);
+  // saludarEstudiantes(estudiantes[i]);
 }
 
 // con for-of
 for (const estudiante of estudiantes) {
+  // saludarEstudiantes(estudiante);
+}
+
+while (estudiantes.length > 0) {
+  var estudiante = estudiantes.shift();
   saludarEstudiantes(estudiante);
 }
+
+// while (estudiante = estudiantes.shift()) {
+//   saludarEstudiantes(estudiante);
+// }
