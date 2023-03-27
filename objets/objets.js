e001919299aeae3ee1fc1146ed63f5412e70950a
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-26 21:05:05 -0300

    funcion constructora

--- objets/objets.js
+++ objets/objets.js
@@ -40,3 +40,21 @@ console.log('miAuto.marca: ' + miAuto.marca);
 console.log('miAuto.annio: ' + miAuto.annio);
 console.log('miAuto.antiguedad: ' + miAuto.antiguedad());
 console.log('miAuto.detalleDelAuto: ' + miAuto.detelleDelAuto());
+
+// funcion constructora
+// es una funcion que fabrica los objectos en base al molde del objecto que le dimos (atributos)
+function Auto(marca, modelo, annio) {
+  this.marca = marca;
+  this.modelo = modelo;
+  this.annio = annio;
+}
+
+console.log(Auto);
+
+// new hace atribuirle autoNuevo el this interno de Auto.
+var autoNuevo = new Auto('Tesla', 'Model 3', 2020);
+var autoNuevo2 = new Auto('Tesla', 'Model X', 2018);
+var autoNuevo3 = new Auto('Tesla', 'Corolla', 2020);
+console.log(autoNuevo);
+console.log(autoNuevo2);
+console.log(autoNuevo3);
