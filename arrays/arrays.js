bd5e78211819b82dfb44abe7d5af833b875b5679
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-03-30 14:55:45 -0300

    cambio el concepto de metodos shift y pop

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -13,29 +13,28 @@ console.log('accediendo a frutas[2]: '+ frutas[2]);
 // methods
 
 console.log('agregando al final uvas y ciruelas');
-// .push() es un metodo que agrega "al final" un elemento
+// Array.push() es un metodo que agrega "al final" un elemento
 var masFrutas = frutas.push('uvas', 'ciruelas');
 console.log(frutas);
 
-console.log('eliminando el ultimo elemento');
-// .pop() elimina el ultimo elemento del array y lo retorna. no tiene parametros (MDN)
+console.log('extrayendo el ultimo elemento');
+// Array.pop() extrae el ultimo elemento del array. por lo tanto se puede entender que retorna el ultimo elemento. No tiene parametros (MDN)
 var ultimo = frutas.pop();
 console.log(frutas);
-console.log('ultimo elemento eliminado: ' + ultimo);
+console.log('ultimo elemento extraido: ' + ultimo);
 
 
-// unshift agrega nuevos elementos al principio del array y retorna nueva longitud
+// Array.unshift() agrega nuevos elementos al principio del array y retorna nueva longitud
 console.log('agregando al principio platano, nispero');
 var nuevaLongitud = frutas.unshift("platano", "nispero");
 console.log('nuevaLongitud: ' + nuevaLongitud);
 console.log(frutas);
 
-// shift es lo mismo que pop pero en vez de borrar el elemento del final, borra el elemento del inicio
-
-console.log('eliminando el primer elemento');
-var borrarFruta = frutas.shift();
+// Array.shift() es lo mismo que Array.pop() pero en vez de extraer el elemento del final, extrae el elemento del inicio
+console.log('extrayendo el primer elemento');
+var extraerFruta = frutas.shift();
 console.log(frutas);
-console.log('primer elemento eliminado: ' + borrarFruta);
+console.log('primer elemento extraido: ' + extraerFruta);
 
 // resumiendo... todo se parece a tratar con pilas... como un cilindro sin tapas
 // donde los elementos estan en el cuerpo del cilindro.
